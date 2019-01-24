a03514c725ba5138ce75a5e7ef832ca6abc5b1a2
William <wulloase95@gmail.com>
2019-01-24 10:01:38 +0100

    mathis

---
+++ minesweeper.js
@@ -0,0 +1,14 @@
+var divArray = []; 
+
+function create_grid(width,height) {
+	var divMaster = document.getElementById('master');
+	console.log(divMaster);
+	for (var i = 0; i < width*height; i++) {
+		var newDiv = $('<div>');
+		newDiv.attr('class','div1' )
+		divArray[i]= newDiv; 
+		$(divMaster).after(newDiv);
+	}
+}
+
+create_grid(10,10);
