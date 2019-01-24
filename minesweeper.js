98c3847f0d7d15f538c86e89bb89c53fd7220614
William <wulloase95@gmail.com>
2019-01-24 11:19:45 +0100

    grille

--- minesweeper.js
+++ minesweeper.js
@@ -1,14 +1,32 @@
 var divArray = []; 
 
+
 function create_grid(width,height) {
 	var divMaster = document.getElementById('master');
 	console.log(divMaster);
-	for (var i = 0; i < width*height; i++) {
-		var newDiv = $('<div>');
-		newDiv.attr('class','div1' )
-		divArray[i]= newDiv; 
-		$(divMaster).after(newDiv);
+	var divGrid = document.getElementById('grid');
+	var tBody = document.getElementById('tbody');
+	
+	for (var i = 0; i < 9; i++) {
+		var newTr = $('<tr>');
+
+		$(tbody).append(newTr);
+		for (var j= 0; j < width; j++) {
+			var newTd = $('<td>');
+			var newDiv = $('<div>');
+			$(newTr).append(newTd);
+			
+			$(newTd).append(newDiv);
+			divArray.push(newDiv);
+			
+		}
+		
+		
+
+	} for (var i = 0; i < divArray.length; i++) {
+		divArray[i].attr('style','background-color:red');
 	}
+
 }
 
-create_grid(10,10);
+create_grid(9,9);
